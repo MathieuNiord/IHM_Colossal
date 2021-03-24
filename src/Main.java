960e4ce2bad099c59d5764448b20de0d9fa37a2a
Gaetan Frejoux <gaetanfrejoux@gmail.com>
2021-03-24 12:15:16 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -14,21 +14,37 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        // le jeu
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(this.getClass().getResource("view/home.fxml"));
-        Parent root = loader.load();
+        // le menu
+        FXMLLoader homeLoader = new FXMLLoader();
+        homeLoader.setLocation(this.getClass().getResource("view/home.fxml"));
+        Parent homeRoot = homeLoader.load();
+        HomeController homeController = homeLoader.getController();
 
-        HomeController c = loader.getController();
+        // le menu
+        FXMLLoader mainLoader = new FXMLLoader();
+        mainLoader.setLocation(this.getClass().getResource("view/main.fxml"));
+        Parent mainRoot = mainLoader.load();
+        MainController mainController = mainLoader.getController();
+
         /*
         Font myFont24 = Font.loadFont(getClass().getResourceAsStream("rainyhearts.ttf"), 24);
         Font myFont48 = Font.loadFont(getClass().getResourceAsStream("rainyhearts.ttf"), 48);
         c.monTitre.setFont(myFont48);
         c.label_Script.setFont(myFont24);
          */
-        primaryStage.setScene(new Scene(root));
+        Stage mainStage = new Stage();
+        mainStage.setScene(new Scene(homeRoot,1260,700));
+        mainStage.setResizable(false);
+        mainStage.showAndWait();
+
+        primaryStage.setScene(new Scene(mainRoot));
         primaryStage.setResizable(false);
         primaryStage.show();
+
+
+
+
+
     }
 
 
--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -11,8 +11,12 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import javafx.util.Pair;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
@@ -46,27 +50,45 @@ public class MainController implements Initializable {
     private GridPane grid_game;
     @FXML
     private ImageView caveman;
+    @FXML
+    private ImageView cat;
 
-    private int cavemanX = 4;
-    private int cavemanY = 4;
+    private HashMap<ImageView,Pair<Integer,Integer>> imageViewPairHashMap;
+
+    public MainController() {
+    }
+
+    private Pair<Integer,Integer> computeNewPair(Pair<Integer,Integer> p,int x, int y){
+        int newK = p.getKey();
+        int newV = p.getValue();
+        if(!(newK+x<1 || newK+x>7)){
+            newK+=x;
+        }
+        if(!(newV+y<1 || newV+y>7)){
+            newV+=y;
+        }
+        return new Pair<>(newK,newV);
+    }
 
     @FXML
     private void cavemanMove(){
         pane_main.setOnKeyPressed(event -> {
 
+            Pair<Integer,Integer> oldPair = imageViewPairHashMap.get(caveman);
+            Pair<Integer,Integer> newPair;
             switch (event.getCode()){
-                case Z:cavemanY--; break;
-                case Q:cavemanX--; break;
-                case S:cavemanY++; break;
-                case D:cavemanX++; break;
-                default:break;
+                case Z:newPair = computeNewPair(oldPair,0,-1) ; break;
+                case Q:newPair = computeNewPair(oldPair,-1,0); break;
+                case S:newPair = computeNewPair(oldPair,0,1); break;
+                case D:newPair = computeNewPair(oldPair,1,0); break;
+                default: newPair = oldPair;break;
             }
-            if(cavemanY<1) cavemanY++;
-            if(cavemanX<1) cavemanX++;
-            if(cavemanY>7) cavemanY--;
-            if(cavemanX>7) cavemanX--;
-            grid_game.getChildren().remove(caveman);
-            grid_game.add(caveman,cavemanX,cavemanY);
+            if(!newPair.equals(imageViewPairHashMap.get(cat)))
+            {
+                imageViewPairHashMap.replace(caveman,newPair);
+                grid_game.getChildren().remove(caveman);
+                grid_game.add(caveman,newPair.getKey(),newPair.getValue());
+            }
         });
     }
 
@@ -87,6 +109,9 @@ public class MainController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        imageViewPairHashMap = new HashMap<>();
+        imageViewPairHashMap.put(caveman,new Pair<>(4,4));
+        imageViewPairHashMap.put(cat,new Pair<>(2,2));
 
         //permet de quitter le jeu
         button_quit.setOnAction(event -> { button_quitAction();});

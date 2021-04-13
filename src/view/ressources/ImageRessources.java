d04b3c4bc446ede717a40297f6bbf4ad91501025
Gaetan Frejoux <gaetanfrejoux@gmail.com>
2021-04-13 19:35:06 +0200

    UPDATES

--- src/view/ressources/ImageRessources.java
+++ src/view/ressources/ImageRessources.java
@@ -7,6 +7,8 @@ public class ImageRessources {
     private ImageRessources() {};
 
     //IMAGES
+    public final static Image IMAGE_BANANA = new Image("assets/images/objects/Banana.png");
+    public final static Image IMAGE_BESCHERELLE = new Image("assets/images/objects/Bescherelle.png");
     public final static Image IMAGE_CAT = new Image("assets/images/characters/cat.gif");
     public final static Image IMAGE_MONKEY = new Image("assets/images/characters/cat.gif");
     public final static Image IMAGE_MOUSE = new Image("assets/images/characters/cat.gif");
@@ -14,7 +16,7 @@ public class ImageRessources {
     public final static Image IMAGE_NAZIS_POSTER = new Image("assets/images/objects/NazisPoster.png");
     public final static Image IMAGE_FLINT = new Image("assets/images/objects/Flint.png");
     public final static Image IMAGE_STICK = new Image("assets/images/objects/Stick.png");
-    public final static Image IMAGE_HEART = new Image("assets/images/objects/ez.gif");
+    public final static Image IMAGE_HEART = new Image("assets/images/environment/heart.pngdaz");
     public final static Image GIF_CAVEMAN = new Image("assets/images/characters/caveman.gif");
     public final static Image IMAGE_CAVEMAN_BACK = new Image("assets/images/characters/cavemanBack.png");
     public final static Image IMAGE_CAVEMAN_FRONT = new Image("assets/images/characters/cavemanFix.png");
@@ -22,7 +24,7 @@ public class ImageRessources {
 
     public final static Image IMAGE_DOOR_UP = new Image("assets/images/place/BasicDoorUp.png");
     public final static Image IMAGE_DOOR_LEFT = new Image("assets/images/place/BasicDoorLeft.png");
-    public final static Image IMAGE_DOOR_RIGHT = new Image("assets/images/place/BasicDoorRight.jpg");
+    public final static Image IMAGE_DOOR_RIGHT = new Image("assets/images/place/BasicDoorRight.png");
     public final static Image IMAGE_DOOR_DOWN = new Image("assets/images/place/BasicDoorDown.jpg");
 
 

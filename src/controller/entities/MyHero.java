5451c26c79069896dccd05808f64cc87377cb26e
laruchsix <61274375+laruchsix@users.noreply.github.com>
2021-04-28 10:31:05 +0200

    bug locker

--- src/controller/entities/MyHero.java
+++ src/controller/entities/MyHero.java
@@ -35,19 +35,23 @@ public class MyHero {
     }
 
     public void addObj(MyEntity obj, Main main, String text, boolean isOnGridGame){
-        this.inventory.add(obj);
 
-        obj.obj_model.take(this.model);
-        obj.obj_model.setDraggableTrue();
-        obj.view.setCursor(Cursor.HAND);
-        this.view.getInvView().getChildren().add(obj.view);
+        if(!inventory.contains(obj)){
+            this.inventory.add(obj);
 
-        if(isOnGridGame){
-            main.removeOnGridPane(obj.view);
-            MyPlace currentPlace =  PLACE_TO_MY_PLACE.get(this.getModel().getPlace());
-            currentPlace.getEntities().remove(obj);
+            obj.obj_model.take(this.model);
+            obj.obj_model.setDraggableTrue();
+            obj.view.setCursor(Cursor.HAND);
+            this.view.getInvView().getChildren().add(obj.view);
+
+            if(isOnGridGame){
+                main.removeOnGridPane(obj.view);
+                MyPlace currentPlace =  PLACE_TO_MY_PLACE.get(this.getModel().getPlace());
+                currentPlace.getEntities().remove(obj);
+            }
+            if(text != null) main.setOnGameLabel(text);
+
         }
-        if(text != null) main.setOnGameLabel(text);
 
 
     }
